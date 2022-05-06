8b371eb481dcecf7a3c4968d68c1f4064be349f0
Maxime Marinel <bourvill@users.noreply.github.com>
2022-05-07 00:03:11 +0200

    Update to latest publish version

--- Package.swift
+++ Package.swift
@@ -1,22 +1,23 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Blog",
+    platforms: [.macOS(.v12)],
     products: [
         .executable(name: "Blog", targets: ["Blog"]),
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
-        .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", .branch("master")),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.9.0"),
+        .package(url: "https://github.com/johnsundell/splashpublishplugin", from: "0.2.0"),
     ],
     targets: [
         .executableTarget(
             name: "Blog",
             dependencies: [
-                "Publish",
-                "SplashPublishPlugin",
+                .product(name: "Publish", package: "publish"),
+                .product(name: "SplashPublishPlugin", package: "splashpublishplugin")
             ]
         ),
     ]
