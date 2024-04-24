3a4a555a1cb39b4165745a3e0bb330e3333431b1
Maxime Marinel <bourvill@users.noreply.github.com>
2024-04-24 23:07:53 +0200

    First build

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://maxime.marinel.me/', // Write here your website url
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
---
+++ src/data/categories.ts
@@ -0,0 +1,10 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Category 1',
+	'Category 2',
+	'Category 3',
+	'Category 4',
+	'Category 5',
+	'Posts',
+	'Thinks'
+] as const
